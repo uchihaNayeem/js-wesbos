0a33759fc6a2e1e3d5fb2f3f3e22277e330dd14a
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-01-06 13:55:33 +0600

    selecting item new way

--- DOM/creating.js
+++ DOM/creating.js
@@ -1,1 +1,22 @@
-console.log('it is working nayeem');
+// console.log('it is working nayeem');
+
+const divs = document.querySelector('div');
+console.log(divs);
+
+const divImage = divs.querySelectorAll('p');
+console.log(divImage);
+
+const allDivs = document.querySelectorAll('div');
+console.log(allDivs);
+
+const items = document.querySelector('.item2');
+console.log(items);
+
+const item2P = items.querySelector('p');
+console.log(item2P);
+
+const newImage = document.querySelector('.newImage');
+console.log(newImage);
+
+const newImage2 = newImage.querySelector('img');
+console.log(newImage2);
