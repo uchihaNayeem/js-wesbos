9cd74cba2f5b54c68f0ae80e1458654dd5ccd5b3
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-01-11 22:29:44 +0600

    add button delete

--- 27. dom-cardio/DOM-Cardio.js
+++ 27. dom-cardio/DOM-Cardio.js
@@ -1,7 +1,202 @@
 
 
-// Make a div-
 
+// Make a div
+
+// add a class of wrapper to it
+
+// put it into the body
+
+// make an unordered list
+
+// add three list items with the words "one, two, three" in them
+// put that list into the above wrapper
+
+// create an image
+
+// set the source to an image
+// set the width to 250
+// add a class of cute
+// add an alt of Cute Puppy
+// Append that image to the wrapper
+
+// with HTML string, make a div, with two paragraphs inside of it
+// put this div before the unordered list from above
+
+// add a class to the second paragraph called warning
+// remove the first paragraph
+
+// create a function called generatePlayerCard that takes in three arguments: name, age, and height
+
+// have that function return html that looks like this:
+// <div class="playerCard">
+//   <h2>NAME — AGE</h2>
+//   <p>They are HEIGHT and AGE years old. In Dog years this person would be AGEINDOGYEARS. That would be a tall dog!</p>
+// </div>
+
+// make a new div with a class of cards
+
+// make 4 player cards using generatePlayerCard
+
+// append those cards to the div
+// put the div into the DOM just before the wrapper element
+// Bonus, put a delete Button on each card so when you click it, the whole card is removed
+
+// select all the buttons!
+// make out delete function
+// loop over them and attach a listener
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* MY CODES Below Final */
+/*  
+
+//  ========= Excersize done by me ====
+// Make a div-
 const myDiv = document.createElement('div');
 
 
@@ -14,9 +209,6 @@ document.body.append(myDiv);
 // make an unordered list-
 const myList = document.createElement('ul');
 
-
-
-
 // add three list items with the words "one, two, three" in them-
 const li1 = document.createElement('li');
 li1.textContent = 'One';
@@ -27,6 +219,7 @@ myList.appendChild(li2);
 const li3 = document.createElement('li');
 li3.textContent = 'Three';
 myList.appendChild(li3);
+
 // put that list into the above wrapper-
 myDiv.append(myList)
 // create an image
@@ -56,7 +249,7 @@ myDiv.insertAdjacentHTML('afterbegin', myHtml);
 
 // add a class to the second paragraph called warning
 const myDiv2 = document.querySelector('div div')
-const secondP = myDiv2.lastElementChild
+const secondP = myDiv2.lastElementChild;
 secondP.classList.add('warning');
 
 // remove the first paragraph
@@ -117,6 +310,7 @@ myBody.insertAdjacentElement('afterbegin', myDiv3);
 
 
 
+*/
 
 
 
@@ -164,170 +358,7 @@ myBody.insertAdjacentElement('afterbegin', myDiv3);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Make a div-
-
-// add a class of wrapper to it-
-
-// put it into the body-
-
-// make an unordered list-
-
-// add three list items with the words "one, two, three" in them-
-// put that list into the above wrapper-
-
-// create an image
-
-// set the source to an image
-// set the width to 250
-// add a class of cute
-// add an alt of Cute Puppy
-// Append that image to the wrapper
-
-// with HTML string, make a div, with two paragraphs inside of it
-// put this div before the unordered list from above
-
-// add a class to the second paragraph called warning
-// remove the first paragraph
-
-// create a function called generatePlayerCard that takes in three arguments: name, age, and height
-
-// have that function return html that looks like this:
-// <div class="playerCard">
-//   <h2>NAME — AGE</h2>
-//   <p>They are HEIGHT and AGE years old. In Dog years this person would be AGEINDOGYEARS. That would be a tall dog!</p>
-// </div>
-
-// make a new div with a class of cards
-
-// make 4 player cards using generatePlayerCard
-
-// append those cards to the div
-// put the div into the DOM just before the wrapper element
-// Bonus, put a delete Button on each card so when you click it, the whole card is removed
-
-// select all the buttons!
-// make out delete function
-// loop over them and attach a listener
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// /* MY CODES Below */
+/* MY CODES Below First TIME */
 
 
 // const myDiv = document.createElement('div');
