b070b0286f3bb36efb276262cf25098ff41df051
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-01-14 22:15:35 +0600

    Get them, Listen for 'some action' & run a func for them

--- 29. Events - Event Listener/Events - Event Listener.js
+++ 29. Events - Event Listener/Events - Event Listener.js
@@ -1,14 +1,31 @@
-// Get them, Listen for them, run a func for them
+// Get them, Listen for 'some action' & run a func for them
+
+
 
 const butt = document.querySelector('.butt');
 const cool = document.querySelector('.cool');
 
-function handleClick(){
-    console.log(`clicked`);
-}
 
+function handleClick(){
+  console.log('clicked');
+}
 
 butt.addEventListener('click', handleClick);
 cool.addEventListener('click', handleClick);
 
-butt.removeEventListener('click', handleClick);
+
+
+cool.removeEventListener('click', handleClick); // this is what we'll call unbinding.
+
+// 10 buttons 
+
+function loopOverFunction(element){
+  console.log(element.path[1].remove());
+}
+
+
+const buy = document.querySelectorAll('.buy');
+// console.dir(butt);
+// buy.addEventListener('click', handleClick);
+
+buy.forEach(buy => buy.addEventListener('click', loopOverFunction));
