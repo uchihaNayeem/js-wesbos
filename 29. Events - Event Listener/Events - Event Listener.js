f5a74ee59f8fa6dedcc7c0994c405f739188c02d
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-01-14 20:20:12 +0600

    event listeners

--- 29. Events - Event Listener/Events - Event Listener.js
+++ 29. Events - Event Listener/Events - Event Listener.js
@@ -1,1 +1,14 @@
-console.log(`it works`);
+// Get them, Listen for them, run a func for them
+
+const butt = document.querySelector('.butt');
+const cool = document.querySelector('.cool');
+
+function handleClick(){
+    console.log(`clicked`);
+}
+
+
+butt.addEventListener('click', handleClick);
+cool.addEventListener('click', handleClick);
+
+butt.removeEventListener('click', handleClick);
