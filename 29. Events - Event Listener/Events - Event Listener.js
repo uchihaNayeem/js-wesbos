3629f46c6735660cd495e360b78182199ad62051
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-01-15 13:39:19 +0600

    Mouse Event & catch what brouser gives us

--- 29. Events - Event Listener/Events - Event Listener.js
+++ 29. Events - Event Listener/Events - Event Listener.js
@@ -17,15 +17,53 @@ cool.addEventListener('click', handleClick);
 
 cool.removeEventListener('click', handleClick); // this is what we'll call unbinding.
 
-// 10 buttons 
 
-function loopOverFunction(element){
-  console.log(element.path[1].remove());
+// // 10 buttons 
+
+// function loopOverFunction(element){
+//   // console.log(element.path[1].remove());
+//   console.log(element);
+//   console.log(`i m button`);
+// }
+
+// const buy = document.querySelectorAll('.buy');
+// // console.dir(butt);
+// // buy.addEventListener('click', handleClick);
+
+// buy.forEach(buy => buy.addEventListener('click', loopOverFunction));
+
+
+
+// multiple buttons 
+
+const buyButtons = document.querySelectorAll('.buy');
+console.log(buyButtons);
+
+// when multiple buttons use forEach(), the method forEach give something for each of the Items
+
+// buyButtons.forEach(function(eachItem){
+//   console.dir(eachItem);
+
+//   eachItem.addEventListener('click', function(){
+//     console.log(`I am button inside of a forEach method`);
+
+//   })
+// })
+
+
+// function insideLoop(){
+//   console.log(`each button clicked`);
+// }
+
+// buyButtons.forEach((buttons) => {
+//   console.log(`hey`);
+//   buttons.addEventListener('click', insideLoop)
+// })
+
+
+function catchWhatBrowserGives(element){
+  console.dir(element);
 }
+buyButtons.forEach(buttons => console.log(buttons));
 
-
-const buy = document.querySelectorAll('.buy');
-// console.dir(butt);
-// buy.addEventListener('click', handleClick);
-
-buy.forEach(buy => buy.addEventListener('click', loopOverFunction));
+buyButtons.forEach(buttons => buttons.addEventListener('click', catchWhatBrowserGives));
