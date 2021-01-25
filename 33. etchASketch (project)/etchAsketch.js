0bab053fc4c88f08d9dcc6a1d8ec256194e90959
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-01-25 08:08:24 +0600

    get the canvas, set context, use Methods

--- 33. etchASketch (project)/etchAsketch.js
+++ 33. etchASketch (project)/etchAsketch.js
@@ -1,1 +1,23 @@
-console.log(`holaaa`);
+// Select the elements on the page - canvas, shake button
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+console.log(ctx);
+const shakeBtn = document.querySelector('.shake');
+
+// let height = canvas.height;
+// let width = canvas.width;
+
+//destructuring
+const {height, width} = canvas;
+
+// Setup our canvas for drawing
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
+ctx.lineWidth = 10;
+
+//to start the drawing
+
+ctx.beginPath(); // start the drawing
+ctx.moveTo(200, 200);
+ctx.lineTo(200, 200);
+ctx.stroke();
