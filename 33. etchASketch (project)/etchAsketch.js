ee6085bcdfd42ed21d9470901a2759b5bd0a4232
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-01-27 07:22:06 +0600

    random Number

--- 33. etchASketch (project)/etchAsketch.js
+++ 33. etchASketch (project)/etchAsketch.js
@@ -1,23 +1,17 @@
-// Select the elements on the page - canvas, shake button
+// we want to draw in canvas using arrow key
+
+//TASKS 1. select the element 2. create a dot in canvas using context 3. create in a random place 4. detect width and height 5. create a handler 6. create a draw function 7. animate the shake button...
+
 const canvas = document.querySelector('#etch-a-sketch');
-const ctx = canvas.getContext('2d');
-console.log(ctx);
 const shakeBtn = document.querySelector('.shake');
+const ctx = canvas.getContext('2d');
+const{height, width} = canvas;
 
-// let height = canvas.height;
-// let width = canvas.width;
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+console.log(x, y);
 
-//destructuring
-const {height, width} = canvas;
-
-// Setup our canvas for drawing
 ctx.lineCap = 'round';
 ctx.lineJoin = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = 30;
 
-//to start the drawing
-
-ctx.beginPath(); // start the drawing
-ctx.moveTo(200, 200);
-ctx.lineTo(200, 200);
-ctx.stroke();
