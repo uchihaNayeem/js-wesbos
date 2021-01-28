046fbe48d35b56bf58063e0ab85542bd87860e0d
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-01-28 23:45:46 +0600

    etchASketch DONE

--- 33. etchASketch (project)/etchAsketch.js
+++ 33. etchASketch (project)/etchAsketch.js
@@ -4,72 +4,73 @@
 
 const canvas = document.querySelector('#etch-a-sketch');
 const shakeBtn = document.querySelector('.shake');
+const MOVE_AMOUNT = 30;
 const ctx = canvas.getContext('2d');
-const{height, width} = canvas;
 
-let x = Math.floor(Math.random() * width);
-let y = Math.floor(Math.random() * height);
-console.log(x, y);
+const {width, height} = canvas;
+let x = Math.floor(Math.random() * width)
+let y = Math.floor(Math.random() * height)
+// console.log(x, y);
+
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
 
 ctx.lineCap = 'round';
 ctx.lineJoin = 'round';
-ctx.lineWidth = 30;
+ctx.lineWidth = MOVE_AMOUNT;
 
 ctx.beginPath();
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
 
-function handleArrow(e){
-  if(e.key.includes('Arrow')){
-    draw({key: e.key})
-    console.log(e.key);
-    e.preventDefault();
-
-  }
-}
-
 function draw({key}){
-  console.log({key});
-  console.log(x, y);
+  console.log(key);
+   hue += 1;
+   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
 
-  if(key === 'ArrowLeft'){
-    x -= 10;
-  }
-  if(key === 'ArrowRight'){
-    x += 10;
-  }
-  if(key === 'ArrowUp'){
-    y -= 10;
-  }
-  if(key === 'ArrowDown'){
-    y += 10;
-  }
-
-  ctx.lineCap = 'round';
-  ctx.lineJoin = 'round';
-  ctx.lineWidth = 30;
   ctx.beginPath();
   ctx.moveTo(x, y);
+
+  switch(key){
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT
+      break;
+    case 'ArrowRight':
+      x += MOVE_AMOUNT
+      break;
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT
+      break;
+    case 'ArrowDown':
+      y += MOVE_AMOUNT
+      break;
+    default:
+      break;
+  }
+  
   ctx.lineTo(x, y);
   ctx.stroke();
-
 }
 
- function clearCanvas() {
+
+function handleArrow(e){
+  
+  if(e.key.includes('Arrow')){
+    draw({key : e.key});
+    e.preventDefault();
+    // console.log(e.key);
+  }
+}
+
+function clearCanvas(){
   canvas.classList.add('shake');
   ctx.clearRect(0, 0, width, height);
-  canvas.addEventListener(
-    'animationend',
-    function() {
-      console.log('Done the shake!');
-      canvas.classList.remove('shake');
-    },
-    { once: true }
-  );
+  canvas.addEventListener('animationend', function(){
+    canvas.classList.remove('shake');
+    console.log(`animation END`);
+  },{once : true});
 }
 
-
-
 window.addEventListener('keydown', handleArrow);
-shakeBtn.addEventListener('click', clearCanvas)
+shakeBtn.addEventListener('click', clearCanvas);
