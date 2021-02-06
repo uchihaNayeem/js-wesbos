7cd4d21f0a58ae20d3172a74866d119ed07a9399
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-02-06 10:57:46 +0600

    IntersectionObservation()

---
+++ 35. Scroll down to accept (project)/scrollToAccept.js
@@ -0,0 +1,8 @@
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+
+function obCallback(payload){
+  console.log(payload);
+}
+
+const ob = new IntersectionObserver(obCallback);
