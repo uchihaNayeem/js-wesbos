b350b86096432a7a2c7c48d5cb01d75998894094
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-02-06 11:42:31 +0600

    IntersectionObservation options

--- 35. Scroll down to accept (project)/scrollToAccept.js
+++ 35. Scroll down to accept (project)/scrollToAccept.js
@@ -1,10 +1,20 @@
 const terms = document.querySelector('.terms-and-conditions');
 const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
 
 function obCallback(payload){
   console.log(payload[0].intersectionRatio);
+  if(payload[0].intersectionRatio === 1){
+    button.disabled = false;
+    console.log(`i am done`);
+    ob.unobserve(watch)
+  }
 }
 
-const ob = new IntersectionObserver(obCallback);
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1
+
+});
 
 ob.observe(watch);
