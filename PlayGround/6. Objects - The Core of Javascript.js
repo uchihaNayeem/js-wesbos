99bc5fa791d67ab311de52f1ff86ccf7c6df20b0
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-02-27 18:09:51 +0600

    colt module 6 objects

---
+++ PlayGround/6. Objects - The Core of Javascript.js
@@ -0,0 +1,48 @@
+/* TOTAL 40m
+
+/* ===== 1. Intro to Objects ====== */
+
+/*
+  [
+    * Objects are collection of properties.
+    * Properties are a key-value Pair.
+    * Rather than accessing data using index, we use custom keys.
+  ]
+*/
+
+/* ===== 2. Creating Object Literals  ====== */
+
+
+const cars = { // [key] : [value] ,
+  name: 'lambo',
+  model: 'aventador',
+  year: 2009,
+  45: 'fortyFive'
+}
+
+//how to access key from a object
+cars.name; // this is valid 
+
+// * Object converts 'keys' to 'string'. 
+// [ cars.45 ] this is invalid. because key(45) is converted to string. 
+
+/* ===== 3. Accessing Object Properties  ====== */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ===== 4. Adding and Updating Properties  ====== */
+/* ===== 5. Nested Arrays & Objects  ====== */
+/* ===== 6. Objects and Reference Types  ====== */
+/* ===== 7. Array/Object Equality ====== */
