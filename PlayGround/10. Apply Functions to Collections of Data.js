3efc8efe5f2f632dd930a2bc4111a54a6f3c60f6
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-04-16 23:54:24 +0600

    colt module 10 advanced func data

--- PlayGround/10. Apply Functions to Collections of Data.js
+++ PlayGround/10. Apply Functions to Collections of Data.js
@@ -758,3 +758,89 @@ const total = [10, 20, 30, 40, 50].reduce((sum, currVal) => {
 /* ======= 12. Even More Reduce! 13:10 ======= */
 /*##############################################################*/
 
+const zVotes = ['y', 'y', 'n', 'y', 'n', 'y', 'n', 'y', 'n', 'n', 'n', 'y', 'y'];
+
+// To tally the zVotes:
+// const results = zVotes.reduce((tally, val) => {
+//   if (tally[val]) {
+//     tally[val]++
+//   } else {
+//     tally[val] = 1;
+//   }
+//   return tally;
+// }, {})
+
+// The shorter version:
+const results = zVotes.reduce((tally, val) => {
+  tally[val] = (tally[val] || 0) + 1;
+  return tally;
+}, {});
+
+const zBooks = [{
+    title: 'Good Omens',
+    authors: ['Terry Pratchett', 'Neil Gaiman'],
+    rating: 4.25,
+    genres: ['fiction', 'fantasy']
+  },
+  {
+    title: 'Changing My Mind',
+    authors: ['Zadie Smith'],
+    rating: 3.83,
+    genres: ['nonfiction', 'essays']
+  },
+  {
+    title: 'Bone: The Complete Edition',
+    authors: ['Jeff Smith'],
+    rating: 4.42,
+    genres: ['fiction', 'graphic novel', 'fantasy']
+  },
+  {
+    title: 'American Gods',
+    authors: ['Neil Gaiman'],
+    rating: 4.11,
+    genres: ['fiction', 'fantasy']
+  },
+  {
+    title: 'A Gentleman in Moscow',
+    authors: ['Amor Towles'],
+    rating: 4.36,
+    genres: ['fiction', 'historical fiction']
+  },
+  {
+    title: 'The Name of the Wind',
+    authors: ['Patrick Rothfuss'],
+    rating: 4.54,
+    genres: ['fiction', 'fantasy']
+  },
+  {
+    title: 'The Overstory',
+    authors: ['Richard Powers'],
+    rating: 4.19,
+    genres: ['fiction', 'short stories']
+  },
+  {
+    title: 'A Truly Horrible Book',
+    authors: ['Xavier Time'],
+    rating: 2.18,
+    genres: ['fiction', 'garbage']
+  },
+  {
+    title: 'The Way of Kings',
+    authors: ['Brandon Sanderson'],
+    rating: 4.65,
+    genres: ['fantasy', 'epic']
+  },
+  {
+    title: 'Lord of the flies',
+    authors: ['William Golding'],
+    rating: 3.67,
+    genres: ['fiction']
+  }
+]
+// To group zBooks by rating: 
+const groupedByRatings = zBooks.reduce((groupedzBooks, book) => {
+  const key = Math.floor(book.rating);
+  if (!groupedzBooks[key]) groupedzBooks[key] = [];
+  groupedzBooks[key].push(book)
+  return groupedzBooks;
+}, {})
