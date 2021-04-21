4674f47ec26cb1bba482da879e5ae8317bdfc665
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-04-21 15:08:21 +0600

    colt module 11 misc

--- PlayGround/11. A Few Miscellaneous JS Features.js
+++ PlayGround/11. A Few Miscellaneous JS Features.js
@@ -63,7 +63,36 @@ multiply(3); //3
 /*############################################*/
 /*  03. Spread for Function Calls 06:53  ====== */  
 /*##############################################*/
+function giveMeFour(a, b, c, d) {
+  console.log('a', a)
+  console.log('b', b)
+  console.log('c', c)
+  console.log('d', d)
+}
 
+const colors = ['red', 'orange', 'yellow', 'green']
+
+// Without spread:
+giveMeFour(colors);
+// a ["red", "orange", "yellow", "green"]
+// b undefined
+// c undefined
+// d undefined
+
+// WITH SPREAD!!!
+// Values are passed as separate args:
+giveMeFour(...colors);
+// a 'red'
+// b 'orange'
+// c 'yellow'
+// d 'green'
+
+//We can also spread strings!
+giveMeFour(...'GOAT');
+// a G
+// b O
+// c A
+// d T
 /*############################################*/
 /*  04. Spread in Array Literals 08:08  ====== */  
 /*##############################################*/
