850a51570c0b055f6fa395ed8b01f77569ba6949
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-04-24 22:47:47 +0600

    colt module 11 misc

--- PlayGround/11. A Few Miscellaneous JS Features.js
+++ PlayGround/11. A Few Miscellaneous JS Features.js
@@ -162,7 +162,20 @@ const random = [...'hello', {
 /*############################################*/
 /*  06. The Arguments Object (not new) 06:48  ====== 
 /*##############################################*/
+//The arguments object is available in every function you write (except arrow functions)
+//It contains all the arguments passed in.
+function sumOne() {
+  //It is NOT an array, we have to turn it into one if we want to use array methods
+  const argsArr = [...arguments]
+  return argsArr.reduce((total, currVal) => {
+    return total + currVal
+  })
+}
 
+// No arguments object inside of arrow functions :(
+const multiply = () => {
+  console.log(arguments);
+}
 /*############################################*/
 /*  07. Rest Parameters (new!) 06:57  ====== */  
 /*##############################################*/
