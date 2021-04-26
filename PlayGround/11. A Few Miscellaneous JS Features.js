b034051ec4c924e1b42d03444898b3423294e71d
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-04-27 00:13:13 +0600

    colt module 11 misc

--- PlayGround/11. A Few Miscellaneous JS Features.js
+++ PlayGround/11. A Few Miscellaneous JS Features.js
@@ -208,7 +208,33 @@ const multiplyTwo= (...nums) => (
 /*############################################*/
 /*  08. Destructuring Arrays 06:16  ====== */  
 /*##############################################*/
+const raceResults= [
+  'Eliud Kipchoge',
+  'Feyisa Lelisa',
+  'Galen Rupp',
+  'Ghirmay Ghebreslassie',
+  'Alphonce Simbu',
+  'Jared Ward'
+];
 
+// The old way:
+// const gold = raceResults[0]
+// const silver = raceResults[1]
+// const bronze = raceResults[2]
+
+// Using Destructuring:
+const [gold, silver, bronze] = raceResults;
+gold; //'Eliud Kipchoge'
+silver; //'Feyisa Lelisa'
+bronze; //'Galen Rupp'
+
+const [first, , , fourth] = raceResults;
+first; //'Eliud Kipchoge'
+fourth; //'Ghirmay Ghebreslassie'
+
+const [winner, ...others] = raceResults;
+winner; //'Eliud Kipchoge'
+others; //["Feyisa Lelisa", "Galen Rupp", "Ghirmay Ghebreslassie", "Alphonce Simbu", "Jared Ward"]
 /*############################################*/
 /*  09. Destructuring Objects 04:16  ====== */  
 /*##############################################*/
