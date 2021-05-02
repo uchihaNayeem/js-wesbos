38247481c26cae50a033fe3c953119fea364c1ed
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-05-02 10:17:56 +0600

    colt module 11 misc DONE

--- PlayGround/11. A Few Miscellaneous JS Features.js
+++ PlayGround/11. A Few Miscellaneous JS Features.js
@@ -1,3 +1,4 @@
+
 //total 1h 5m
 
 /*############################################*/
@@ -11,7 +12,6 @@
                      * default parameters
   ]
 */
-
 
 /*############################################*/
 /* ======= 02. Default Parameters 07:58  ====== */  
@@ -31,7 +31,10 @@ let greet = (person, greeting = 'Hi', puntuation = '!')=>{
 /*############################################*/
 /*  03. ======= Spread for Function Calls 06:53  ====== */  
 /*##############################################*/
-console.log(`object`);
+console.log(`
+======= Spread for Function Calls 06:53  ====== 
+
+`);
 /*
   [
     1. spread syntax, [ ... ]
@@ -72,8 +75,10 @@ const bolodama = [' olodama', ' rasengan', ' mangekyo', ' sharingan']
 /*############################################*/
 /*  04. ======= Spread in Array Literals 08:08  ====== */  
 /*##############################################*/
-console.log(`04. ======= Spread in Array Literals 08:08  ====== `);
+console.log(`
+04. ======= Spread in Array Literals 08:08  ====== 
 
+`);
 
 /*
   [ [i get scared when i hear these term XD ]
@@ -83,13 +88,11 @@ console.log(`04. ======= Spread in Array Literals 08:08  ====== `);
   ]
 */
 
-
 const engDepts = ['cse', 'eee', 'ete']
 const phyDepts = ['atomic physics', 'astro physics', 'metarial physics']
 
 const allDepts = ['biology',...engDepts, ...phyDepts]
 console.log(allDepts);
-
 
 //creates a whole new arary.
 const copyEngDept = [...engDepts];
@@ -102,162 +105,232 @@ let powerArrayOne = power.split('')
 
 //copy this in console to see what happens --> [...'olodama', ...'rasendan']
 
+//by using `.concat` can also make one array with two other array, but
+//spread is better
+let someNewPet = ['russell', 'crug', 'tomRiddle', 'lordVoldomord']
+let someNewPetOne = ['kurama', 'shikaku', 'madaraio', 'hakkirariu']
+let someNewPetTwo= ['pikachu', 'articuno', 'zapdos', 'miwTwoo']
 
+let newAllPetToday = someNewPet.concat(someNewPetOne, someNewPetTwo)
+console.log(newAllPetToday);
 
 /*############################################*/
-/*  05. Spread in Object Literals 07:49  ====== */  
+/*  05. ======= Spread in Object Literals 07:49  ====== */  
 /*##############################################*/
-const feline = {
+console.log(`
+05. ======= Spread in Object Literals 07:49  ======
+
+`);
+
+/*
+  [
+    1. in object destructuring, if there is same property name
+       then it replace the last property value.
+    2. 
+  ]
+*/
+
+let myDog = {
+  color: 'blue',
   legs: 4,
-  family: 'Felidae'
-};
-
-const canine = {
-  family: 'Caninae',
-  furry: true,
-  legs: 4
-};
-
-const dog = {
-  ...canine,
-  isPet: true,
-  adorable: true
-}
-//{family: "Caninae", furry: true, legs: 4, isPet: true, adorable: true}
-
-const houseCat = {
-  ...feline,
-  isGrumpy: true,
-  personality: 'unpredictable'
-}
-//{legs: 4, family: "Felidae", isGrumpy: true, personality: "unpredictable"}
-
-const catDog = {
-  ...canine,
-  ...feline
-}
-//{family: "Felidae", furry: true, legs: 4}
-
-//Order matters! Legs will be 3 here, because we set it AFTER spreading canine.
-const tripod = {
-  ...canine,
-  legs: 3,
-}
-//{family: "Caninae", furry: true, legs: 3}
-
-const catDogClone = {
-  ...catDog
+  isFurry: true
 }
 
-const random = [...'hello', {
-  ...catDog
-}]
+let myTiger ={
+  color: 'yellow',
+  isHungry: true,
+  age: 20,
+  legs: 5
+}
+
+let myPet = {
+  ...myDog,
+  ...myTiger,
+  pet: 'olodama'
+}
+
+
+let newPet = 'tigerRoyal'
+
+console.log(myPet);
+
+//object destructuring in an array
+let petArr = [{...myDog}, ...newPet]
+console.log(petArr);
+
 /*############################################*/
-/*  06. The Arguments Object (not new) 06:48  ====== 
+/*  06. ======= The Arguments Object (not new) 06:48  ====== */
 /*##############################################*/
-//The arguments object is available in every function you write (except arrow functions)
-//It contains all the arguments passed in.
-function sumOne() {
-  //It is NOT an array, we have to turn it into one if we want to use array methods
-  const argsArr = [...arguments]
-  return argsArr.reduce((total, currVal) => {
-    return total + currVal
+console.log(`
+06. ======= The Arguments Object (not new) 06:48  ======
+
+`);
+/* WE WILL NOT USE 'THE ARGUMENT OBJECT' TO WORK WITH 
+UNLIMITED 'PARAMETER'. [ WE'LL USE REST \m/ ]. 
+
+*/
+/*
+  [ argument is 'array like object'
+    1. Its a old way to do stuff with unlimited parameters 
+    2. the arguments object gives an array of parameters.
+    3. is a old way.
+    4. --> 'argument' will not work in arraw function!!!
+
+  ]
+*/
+
+//here get all the passed numder in the 'argument object'
+function someTotalNum(){
+  let total =0;
+  for(let i = 0; i<arguments.length;i++){
+    total += arguments[i]
+  }
+  console.log(total);
+}
+
+someTotalNum(12,23,45,67,79,21)
+
+
+//to run a array methon we must spread it in an array
+function anotherTotal(){
+  let arr = [...arguments]
+ let total= arr.reduce((acc, currentNum)=>{
+    return acc + currentNum
   })
+  console.log(total);
+}
+anotherTotal(12,23,45,67,79,21)
+
+/* 
+   arguments gives all the parameter in the 
+   array, If we want to remove the defined 
+   parameter, We'll use slice() 
+*/
+function greatNameTitleMe(first, last) {
+  let arg = [...arguments]
+  let sliceing = arg.slice(2, 4)
+  console.log(sliceing);
+  console.log(first, last, arguments);
+
 }
 
-// No arguments object inside of arrow functions :(
-const multiplyOne = () => {
-  console.log(arguments);
-}
+greatNameTitleMe('nayeem', 'uddin', 'the Olodama', 'the rasengan')
+
+
 /*############################################*/
-/*  07. Rest Parameters (new!) 06:57  ====== */  
+/*  07. ======= Rest Parameters (new!) 06:57  ====== */  
 /*##############################################*/
-// OLD WAY!
-// function sum() {
-//   const argsArr = [...arguments]
-//   return argsArr.reduce((total, currVal) => {
-//     return total + currVal
-//   })
-// }
+console.log(`
+07. ======= Rest Parameters (new!) 06:57  ======
 
-// New way using rest:
-function sumOne(...nums) {
-  return nums.reduce((total, currVal) => {
-    return total + currVal
+`);
+
+/*
+  [
+    1. rest gives us an array of remaining parameters.
+    2. ...rest used in function 
+    3. we can use array methods directly in rest oparator.
+  ]
+*/
+
+
+function makeATotal(...nums){ 
+  console.log(nums);
+  let total= nums.reduce((acc , currentNum)=>{
+    return acc + currentNum
   })
+  console.log(total);
 }
 
-//We can have named params and then collect the rest into an array:
-function fullNameOne(first, last, ...titles) {
-  console.log('first', first)
-  console.log('last', last)
-  console.log('titles', titles)
+makeATotal(54,76,347,634,76,34,8734)
+makeATotal(1,2,3,4,5,6,7)
+
+
+
+function kingTitles(first, last, ...titles){
+  console.log(`${first} is first name`);
+  console.log(`${last} is last name`);
+  console.log(`${titles} this is an array of remaining parameters`);
 }
 
-// We can use rest parameters in arrow functions!
-const multiplyTwo= (...nums) => (
-  nums.reduce((total, currVal) => total * currVal)
-)
+
+// rest(...rest) works in arrow function
+let multiplyNumbers = (...numbers) => {
+  return numbers.reduce((acc, currentNum)=> acc * currentNum)
+}
+console.log(`multiply ans: ${multiplyNumbers(12,2)}`);
+
+
 /*############################################*/
-/*  08. Destructuring Arrays 06:16  ====== */  
+/*  08. ======= Destructuring Arrays 06:16  ====== */  
 /*##############################################*/
-const raceResults= [
-  'Eliud Kipchoge',
-  'Feyisa Lelisa',
-  'Galen Rupp',
-  'Ghirmay Ghebreslassie',
-  'Alphonce Simbu',
-  'Jared Ward'
-];
+console.log(`
+08. ======= Destructuring Arrays 06:16  ======
 
-// The old way:
-// const gold = raceResults[0]
-// const silver = raceResults[1]
-// const bronze = raceResults[2]
+`);
 
-// Using Destructuring:
-const [gold, silver, bronze] = raceResults;
-gold; //'Eliud Kipchoge'
-silver; //'Feyisa Lelisa'
-bronze; //'Galen Rupp'
+let shinobiPower = [
+  'olodama',
+  'sharingan',
+  'mankgekyo',
+  'rasengan',
+  'shinra tense',
+  'gamabunta',
+]
 
-const [first, , , fourth] = raceResults;
-first; //'Eliud Kipchoge'
-fourth; //'Ghirmay Ghebreslassie'
+let [naruto, , itachi, jiraiya] = shinobiPower;
+let [,,shikimaru, ...others] = shinobiPower // extra comma(,) escaping index
 
-const [winner, ...others] = raceResults;
-winner; //'Eliud Kipchoge'
-others; //["Feyisa Lelisa", "Galen Rupp", "Ghirmay Ghebreslassie", "Alphonce Simbu", "Jared Ward"]
+console.log(shikimaru, others);
+
+
 /*############################################*/
-/*  09. Destructuring Objects 04:16  ====== */  
+/*  09. ======= Destructuring Objects 04:16  ====== */  
 /*##############################################*/
-const runnerOne = {
-  firstOne: "Eliud",
-  last: "Kipchoge",
-  country: "Kenya",
-  title: "Elder of the Order of the Golden Heart of Kenya"
+console.log(`
+09. ======= Destructuring Objects 04:16  ======
+
+`);
+
+//01
+let playerInfo = {
+  name: 'Ronaldo',
+  height: '6 feet 4',
+  age: 30,
+  country: 'Portugal'
+
 }
 
-// const {
-//   first,
-//   last,
-//   time
-// } = runner;
+let {name, age, ...other} = playerInfo
+console.log(name, age, other);
 
-const {
-  country: nation,
-  title: honorific
-} = runnerOne;
+//rename the property with destructuring
+let {name: nam, age: boyosh} = playerInfo
+console.log(name, boyosh);
 
-const {
-  firstOne,
-  last,
-  ...other
-} = runnerOne;
+
+//02 
+let morePlayerInfo = {
+  ...playerInfo,
+  isMarried: true,
+  kids: 1
+}
+
+//here I've to rename the property to get the data becase 'name & age'
+//already decleared.
+let {name: moreName, age: moreAge} = morePlayerInfo;
+console.log(moreName, moreAge);
+
 /*############################################*/
-/*  10. Nested Destructuring 02:54  ====== */  
+/*  10. ======= Nested Destructuring 02:54  ====== */  
 /*##############################################*/
-const resultsOne = [{
+console.log(`
+10. ======= Nested Destructuring 02:54  ======
+
+`);
+
+
+const merathonResults = [{
   first: "Eliud",
   last: "Kipchoge",
   country: "Kenya",
@@ -273,18 +346,29 @@ const resultsOne = [{
   country: 'United States'
 }
 ]
-// NESTED DESTRUCTURING
-const [{
-first: goldWinner
-}, {
-country
-}] = resultsOne;
-goldWinner; //"Eliud"
-country; //"Ethiopia"
+
+
+let [,silver, bronge] = merathonResults;
+console.log(silver);
+
+let {first, country} = silver
+console.log(first, country);
+
+//we've to rename, because `first` is taken
+let{ first: namai, last: lamai} = bronge
+console.log(namai, lamai );
+
+
 /*############################################*/
-/*  11. Destructuring Parameters 05:04  ====== */  
+/*  11. ======= Destructuring Parameters 05:04  ====== */  
 /*##############################################*/
-const runner = {
+console.log(`
+11. ======= Destructuring Parameters 05:04  ======
+
+`);
+
+//01
+const runnerMan = {
   first: "Eliud",
   last: "Kipchoge",
   country: "Kenya",
@@ -292,24 +376,21 @@ const runner = {
 }
 
 // Rather than destructuring INSIDE the function body
-// function print(person) {
-//   const {
-//     first,
-//     last,
-//     title
-//   } = person;
-//   console.log(`${first} ${last}, ${title}`)
-// }
+function greetRunner(person){
+  let {first, last, title} = person;
+  console.log(`Name: ${first} Last: ${last} Title: ${title}`);
+}
+greetRunner(runnerMan)
 
 // We can unpack the values we want right in the parameter list:
-function print({
-  first,
-  last,
-  title
-}) {
-  console.log(`${first} ${last}, ${title}`)
+function greetRunnerOne({first, last, title}){
+  console.log(`Name: ${first} Last: ${last} Title: ${title}`);
 }
 
+greetRunnerOne(runnerMan);
+
+
+//02
 const response = [
   'HTTP/1.1',
   '200 OK',
@@ -317,7 +398,12 @@ const response = [
 ]
 
 // Also works with array parameters:
-function parseResponse([protocol, statusCode, contentType]) {
-  console.log(`Status: ${statusCode}`)
+function webStatus([website, status, format]){
+  console.log(`
+  Link: ${website}
+  Status: ${status}
+  format: ${format}
+  `);
 }
 
+webStatus(response);
