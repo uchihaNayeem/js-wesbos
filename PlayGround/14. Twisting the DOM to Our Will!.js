c1b9a4ef7bae3b9dd1710e33f18d82fff06518df
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-08-23 01:03:46 +0600

    module 14 dom in depth

--- PlayGround/14. Twisting the DOM to Our Will!.js
+++ PlayGround/14. Twisting the DOM to Our Will!.js
@@ -1,12 +1,30 @@
 //total 2h 7m
 
 
-
  
 /* ##################################################### */
  /* ========= Working with innerText & textContent  10:52 =========== */
  /*###################################################  */
     
+
+    /* 
+        [ -- WE'll use innerText -- 
+          1. innerText and textContent both are similer
+             gives us the text/content inside of it.
+          2. but there is a big difference here
+              * .innerText gives us value showed in the website.
+              *.textContent gives everything. if a text is 
+                set to display:none. it gies that too
+        ]
+    */
+        
+        let h1One = document.querySelector('.header');
+        h1One.innerText = 'Text changed with js'
+
+        let pOne = document.querySelector('.main')
+        console.log(pOne.innerText)
+        console.log(pOne.textContent)
+
  /* ##################################################### */
  /* ========= innerHTML  07:12 =========== */
  /*###################################################  */
