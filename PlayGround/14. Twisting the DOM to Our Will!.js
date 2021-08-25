9dae048612df40bfc812c0eca707c0825c1e9bc1
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-08-25 08:18:01 +0600

    colt module 14 dom

--- PlayGround/14. Twisting the DOM to Our Will!.js
+++ PlayGround/14. Twisting the DOM to Our Will!.js
@@ -41,51 +41,55 @@
   myH1.innerHTML += "<span class='customColor'> !!!</span>"
   myH1.innerText += "<b> !!! </b>" // it doresnot reconnize the HTML. put like a text;
 
+  //note: span tag er class ta update er por kaj kortesena. karon .innerHtml ta .innerText hoye gese
   
-  console.clear()
+
 
  /* ##################################################### */
  /* ========= value, src, href, and more  09:37 =========== */
  /*###################################################  */
 
   
+  //to get all the input. gives an array
+  let myInputs = document.querySelectorAll('#playWithAttribute input');
+  
+  /* to get the value fro input tags */
+  myInputs[0].value
+  myInputs[1].value
+  myInputs[2].value
+  // for checkbox use .checked
+  myInputs[3].checked     
 
+  /* set values and empty string */
+  myInputs[1].placeholder = 'please give pass'
 
+  //empty the value
+  myInputs[0].value = ''
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  /* for links */
+  let myLink= document.querySelector('#playWithAttribute a'); 
+  myLink.href = "https://www.google.com"
+  /* for image */
+  let myImg= document.querySelector('#playWithAttribute img'); 
+  myImg.src = "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
 
 
 
  /* ##################################################### */
- /* ========= Getting & Setting Attributes  03:30 =========== */
+ /* ======== Getting & Setting Attributes  03:30 ======== */
  /*###################################################  */
 
+
+  let inputRange = document.querySelector('input[name="myRange"]')
+
+  //get attribut and its value
+  inputRange.getAttribute('max'); //it gives the attribute value
+  
+  //set attribute and assign value
+  inputRange.setAttribute('step', '1000')
+
+
+  console.clear()
  /* ##################################################### */
  /* ========= Finding Parent/Children/Siblings  06:27 =========== */
  /*###################################################  */
