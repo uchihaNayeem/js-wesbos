876e43abebf8781faf0fabac95f94cf4690e723f
uchihaNayeem <uchiha.nayeem@gmail.com>
2022-02-24 09:01:53 +0600

    >=

--- PlayGround/SELF PRACTICE 2022/4. Controlling Program Logic and Flow.js
+++ PlayGround/SELF PRACTICE 2022/4. Controlling Program Logic and Flow.js
@@ -1,6 +1,37 @@
 /*====================================*/
 /* ====== 2. Comparison Operators ====== */
 /*====================================*/
+/* Greater than >  */
+ 
+console.log(10 > 8, 'true') 
+console.log(98 > 654, 'false')
+console.log(65 > 54, 'true')
+console.log(45 > 87, 'false')
+console.log(21 > 45, 'false')
+console.log(54> 85, 'false')
+console.log(52 >  20, 'true')
+console.log(52 > 65, 'false')
+console.log(6698 > 654, 'true')
+
+console.clear()
+
+/* Less than < */
+
+console.log(65 < 96, "true")
+console.log(65 < 52, "false")
+console.log(32 < 45, "true")
+console.log(98 < 25, 'false')
+console.log(45 < 89, 'true')
+console.log(12 < 65, 'true')
+console.log(78 < 78, 'false')
+console.log(65 < 19, 'false')
+console.log(31 < 35, 'true')
+console.log(87 < 85, 'false')
+ 
+
+
+
+
 
 
 /*====================================*/
@@ -31,6 +62,27 @@ console.log(`this is (0 == '') (empty string) gives -> ${(0 == '')} `); // true
 /* 6. ====== If Statements ====== */
 /* 7. ====== Else If ====== */
 /*====================================*/
+
+
+let rating = 3;
+
+if (rating ){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //FIND the number if it's ODD or EVEN 
