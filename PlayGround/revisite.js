1e3bec054a8b14ce79c90e1ae4932ebb1324017f
uchihaNayeem <uchiha.nayeem@gmail.com>
2022-10-15 23:52:42 +0600

    secret message 23 module

---
+++ PlayGround/revisite.js
@@ -0,0 +1,145 @@
+/* let newLine = "HELOO I LOVE YOU";
+
+let testOne = newLine.slice(4);
+let testTwo = newLine.slice(8,12)
+
+console.log(testOne);
+console.log(testTwo);
+
+
+let funny = 'ha ha tee tee ha'
+let testThree = funny.replace('tee', 'ha')
+console.log(testThree)
+
+let testFour = funny.replace(/\a/g, 'e');
+console.log(testFour)
+
+
+// lower case er 't' change hocche na
+
+let myTea = 'tee tee tee tee'
+let testFive = myTea.replace(/\t/g, 'h')
+console.log(testFive)
+
+
+
+
+
+ */       // shift + alt + a for commenting
+
+
+
+
+/* 
+
+
+
+
+let firstName = 'Nayeem';
+let lastName = 'Uddin'
+let person = `${firstName} ${lastName} is going to give his project alone.`
+console.log(person)
+
+//null and undefined
+let myAcc = undefined;
+let myAccOne = null;
+
+console.log(myAcc)
+console.log(myAccOne)
+
+// math oparator
+
+let num1 = Math.floor(Math.random()*6 + 1)
+
+// console.log(num1)
+
+
+
+let numbers = [,1,2,6,5,4,5,6,4,4,5,66,]
+
+for(let i = 0; i<numbers.length; i++){
+    let num1 = Math.floor(Math.random()*6 + 1)
+    console.log('random Nums:',num1)
+}
+
+// for(let i = 0; i<= 10; i++){
+//     console.log(`we get: ${i}`);
+//   }
+  
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
